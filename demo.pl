41ad10866be693316a22cc878ac1df76a48cb043
Piotr Janczyk <pjanczyk97@gmail.com>
2020-11-09 13:53:01 +0100

    Update demo

--- demo.pl
+++ demo.pl
@@ -1,10 +1,16 @@
 :- use_module(commands).
 
-:- commands:init_board([3, 3]).
+%     1 2 3 4
+%   1 . . . .
+%   2 . y x .
+%   3 x y y .
+%   4 z . . .
+:- commands:init_board([4, 4]).
 :- commands:put_tile([2, 2], y).
 :- commands:put_tile([3, 3], y).
 :- commands:put_tile([3, 2], x).
 :- commands:put_tile([1, 3], x).
 :- commands:put_tile([2, 3], y).
+:- commands:put_tile([1, 4], z).
 
 :- commands:suggest_moves.
