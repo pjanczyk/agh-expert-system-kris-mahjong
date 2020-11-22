a089287a935daeedc4de6b95f996f08d5b080696
Piotr Janczyk <pjanczyk97@gmail.com>
2020-11-23 00:15:57 +0100

    Improve demo

--- prolog/demo.pl
+++ prolog/demo.pl
@@ -4,8 +4,18 @@
   % 1  2  3  4
   [ a, ♥, c, ♥ ], % 1
   [ ♦, ♦, g, h ], % 2
-  [ i, ♣, -, ♣ ], % 3
+  [ ♥, -, ♥, l ], % 3
   [ m, n, o, p ]  % 4
 ]).
 
-:- api:suggest_moves(Moves), print(Moves).
+:- api:suggest_moves(Moves), print(Moves), nl.
+
+:- api:remove_tiles([1, 2], [2, 2]).
+
+:- api:suggest_moves(Moves), print(Moves), nl.
+
+:- api:remove_tiles([2, 1], [4, 1]).
+
+:- api:suggest_moves(Moves), print(Moves), nl.
+
+:- api:remove_tiles([1, 3], [3, 3]).
